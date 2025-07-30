9615d271c4973e4ee3062f337a8636ec17d7fadb
joshpcausey <joshpcausey@gmail.com>
2025-07-30 15:24:51 -0500

    send event to query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,6 +14,9 @@ app.post('/events', (req, res) => {
   axios
     .post('http://localhost:4001/events', event)
     .catch((err) => console.log(err));
+  axios
+    .post('http://localhost:4002/events', event)
+    .catch((err) => console.log(err));
 
   res.send({ status: 'OK' });
 });
